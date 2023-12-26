68cad9cc708a19181609ada0cbd47b19de79887a
Brilliant Ambuj <76522659+ambuj1211@users.noreply.github.com>
2023-12-27 00:21:54 +0530

    change3

--- script.js
+++ script.js
@@ -63,6 +63,3 @@ function calculateTan() {
     var angle = Math.PI/180*parseFloat(result.value);
     result.value = Math.tan(angle);
 }
-document.addEventListener("contextmenu", (event) => {
-           event.preventDefault();
-        });
